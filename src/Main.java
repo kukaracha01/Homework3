7311831ec9f550e959cc9d421c868a78676b992e
mu <semkadarov@gmail.com>
2024-03-26 21:53:01 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+       byte theSmallest = 127;
+       System.out.println("Значение переменной theSmallest с типом byte равно " + theSmallest);
+       short nextAfterTheSmallest = 32767;
+       System.out.println("Значение переменной nextAfterTheSmallest с типом short равно " + nextAfterTheSmallest);
+       int theThirdYoungest = 32768;
+       System.out.println("Значение переменной theThirdYoungest с типом int равно " + theThirdYoungest);
+       long theRemainingOne = 77L;
+       System.out.println("Значение переменной theRemainingOne с типом long равно " + theRemainingOne);
+       float startWithADot = 12.33F;
+       System.out.println("Значение переменной startWithADot с типом float равно " + startWithADot);
+       double endWithADot = 4.74858965654;
+       System.out.println("Значение переменной endWithADot с типом double равно " + endWithADot);
+
+
+
+
+    }
+}
